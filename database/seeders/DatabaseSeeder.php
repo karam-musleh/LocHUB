fd3c86906801517fbe747d797e5fc6f2e6504abf
karam-musleh <kk420271025@gmail.com>
2026-03-31 13:38:56 +0300

    Add additional user seeding for Admin User in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
